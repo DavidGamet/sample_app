6705e0dedf6215f6f3f6208ce8fda9cdb50671a3
pc <atelierdufilm@orange.fr>
2018-01-12 22:45:59 +0100

    account debut

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180112154654) do
+ActiveRecord::Schema.define(version: 20180112214212) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180112154654) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
 end
