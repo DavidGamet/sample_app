ebc9d785c761c09ef572d31e1393523efe4f008c
pc <atelierdufilm@orange.fr>
2018-01-12 16:58:24 +0100

    upadate etc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index]
 
   def new
     @user = User.new
   end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
 
   def show
     @user = User.find(params[:id])
@@ -19,12 +25,34 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+    def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Your Profile is Update !"
+      render 'show'
+    else
+      render 'edit'
+    end
+  end
+
 
 private
 
 def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
+
+# Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+    end
+  end
 
 
 end
